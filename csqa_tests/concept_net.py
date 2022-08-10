89287a9aa4d9c8a944b6f99ddaafc21061ace157
Sondre Wold <sondrewold1@gmail.com>
2022-08-10 15:42:40 +0200

    Add graph maker

---
+++ csqa_tests/concept_net.py
@@ -0,0 +1,22 @@
+from pykeen.pipeline import pipeline
+import matplotlib.pyplot as plt
+import torch
+from pykeen.datasets import Nations, Countries
+from pykeen.pipeline import pipeline
+from typing import List
+
+
+result = pipeline(
+    dataset='ConceptNet',
+    model='PairRE',
+    # Training configuration
+    training_kwargs=dict(
+        num_epochs=50,
+        use_tqdm_batch=True,
+    ),  
+    # Runtime configuration
+    random_seed=1235,
+    device='gpu',
+)
+
+pipeline_result.save_to_directory('graph')
